d7b4154124751f06b126673b389d1ec27bc7ffd9
KaidoDev <igor.tuchel0@gmail.com>
2025-10-26 16:53:29 +0000

    added catppuccin

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppucin",
+    priority = 1000,
+  },
+  {
+    "Lazyvim/Lazyvim",
+    opts = {
+      colorscheme = "catppuccin-macchiato",
+    },
+  },
+}
